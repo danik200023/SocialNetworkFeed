143ea3ad9aad44212e89e5d8d328ce47ac4bade6
Danila Umnov <umn0v.danila@yandex.ru>
2025-03-05 13:01:51 +0400

    Initial Commit

---
+++ SocialNetworkFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialNetworkFeed
+//
+//  Created by Danila Umnov on 05.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
